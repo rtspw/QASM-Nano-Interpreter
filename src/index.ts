a8acc248aa4a0441b61dec09f0138ae1f65508fe
rtspw <richard.cx.tang@gmail.com>
2022-11-16 23:26:53 -0800

    Add command line argument parsing with verbose option

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,35 @@
-import * as fs from 'fs'
 import * as path from 'path'
+import yargs from 'yargs'
 
 import QASMRuntime from './qasm-runtime'
 
-const fileArg = process.argv?.[2] ?? ''
+const argv = yargs
+  .usage('$0 <filename>')
+  .version('0.0.1')
+  .help()
+  .alias('h', 'help')
+  .option('verbose', {
+    alias: 'v',
+    default: false,
+    type: 'boolean',
+    description: 'Output state and operation of each step in simulation',
+  })
+  .demandCommand(1)
+  .strictOptions()
+  .showHelpOnFail(true)
+  .parseSync()
+
+const fileArg = process.argv[2]
 const filename = path.join(__dirname, '../', fileArg)
-const runtime = new QASMRuntime({ verbose: true })
+const runtime = new QASMRuntime({ verbose: argv.verbose })
 
 try {
   console.log(runtime.executeFromFile(filename).toString())
 } catch (err: any) {
   if (err.code === 'ENOENT') {
     console.error(`File '${filename}' does not exist`)
+  } else if (err.code === 'EISDIR') {
+    console.error(`File ${filename} is a directory`)
   } else {
     throw err
   }
